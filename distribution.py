be869f602a444df6badee00f97f24432f0b33421
Sasha <89406024+Kozak-Alexandr@users.noreply.github.com>
2023-10-16 17:23:27 +0300

    Add files via upload

---
+++ distribution.py
@@ -0,0 +1,55 @@
+import numpy as np
+
+class LaplaceDistribution:    
+    @staticmethod
+    def mean_abs_deviation_from_median(x: np.ndarray):
+        '''
+        Args:
+        - x: A numpy array of shape (n_objects, n_features) containing the data
+          consisting of num_train samples each of dimension D.
+        '''
+        ####
+        # Do not change the class outside of this block
+        median = np.median(x, axis=0)
+        abs_deviation = np.abs(x - median) 
+        mean_abs_deviation = np.mean(abs_deviation, axis=0)
+        return mean_abs_deviation
+        # Your code here
+        ####
+
+    def __init__(self, features):
+        '''
+        Args:
+            feature: A numpy array of shape (n_objects, n_features). Every column represents all available values for the selected feature.
+        '''
+        ####
+        # Do not change the class outside of this block
+        self.loc = np.median(features, axis=0)
+        self.scale = np.sum(np.abs(features - self.loc), axis=0) / features.shape[0]
+        print (features.shape)
+
+        #self.features = features
+    
+        ####
+
+
+    def logpdf(self, values):
+        '''
+        Returns logarithm of probability density at every input value.
+        Args:
+            values: A numpy array of shape (n_objects, n_features). Every column represents all available values for the selected feature.
+        '''
+        ####
+        # Do not change the class outside of this block
+        return -np.abs(values - self.loc) / self.scale - np.log(2 * self.scale)
+
+        ####
+        
+    
+    def pdf(self, values):
+        '''
+        Returns probability density at every input value.
+        Args:
+            values: A numpy array of shape (n_objects, n_features). Every column represents all available values for the selected feature.
+        '''
+        return np.exp(self.logpdf(values))
